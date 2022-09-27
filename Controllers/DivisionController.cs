437ccf42d4004b9221e808b3f515ca7e3831ba3f
Santana118 <zeozaqy@gmail.com>
2022-09-27 07:46:00 +0700

    Add new method for CRUD process

--- Controllers/DivisionController.cs
+++ Controllers/DivisionController.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleWebMVC.Context;
 using SimpleWebMVC.Models;
 using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-
+using System.Linq;
 
 namespace SimpleWebMVC.Controllers
 {
@@ -12,148 +11,73 @@ namespace SimpleWebMVC.Controllers
     public class DivisionController : Controller
 
     {
-        SqlConnection conn;
-        public string connectionString =
-            "Data Source=WINDOWS-PC;" +
-            "Integrated Security=True;" +
-            "Connect Timeout=30;" +
-            "Encrypt=False;" +
-            "TrustServerCertificate=False;" +
-            "ApplicationIntent=ReadWrite;" +
-            "MultiSubnetFailover=False;" +
-            "Initial Catalog=KantorSederhana;" +
-            "User ID=WINDOWS-PC\\Santana;" +
-            "Password=mateyize49;";
+        MyContext myContext;
+        public DivisionController(MyContext myContext)
+        {
+            this.myContext = myContext;
+        }
+
         public IActionResult Index()
         {
-            string query = "SELECT * FROM Division ORDER BY id ASC;";
-            conn = new SqlConnection(connectionString);
-            SqlCommand sqlCommand = new SqlCommand(query, conn);
-            List<Division> Divisions = new List<Division>();
-            try
-            {
-                conn.Open();
-                using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
-                {
-                    if (sqlDataReader.HasRows)
-                    {
-                        while (sqlDataReader.Read())
-                        {
-                            Division division = new Division();
-                            division.Id = Convert.ToInt32(sqlDataReader[0]);
-                            division.DivisionName = sqlDataReader[1].ToString();
-                            Divisions.Add(division);
-                            Console.WriteLine("{0} {1}", sqlDataReader[0], sqlDataReader[1]);
-                        }
-                    }
-                    //else
-                    //{
-                    //    //Console.WriteLine("Wrong password or username !!");
-                    //}
-                    sqlDataReader.Close();
-                }
-                conn.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-            ViewBag.LDivision = Divisions;
-            return View();
+            var data = myContext.Divisions.ToList();
+            return View(data);
         }
-        public IActionResult AddDivision(string id, string _)
+
+        public IActionResult FindId(int id)
         {
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            Division data = myContext.Divisions.Find(id);
+            if (data == null)
             {
-                conn.Open();
-                SqlTransaction sqlTransaction = conn.BeginTransaction();
-
-                SqlCommand sqlCommand = conn.CreateCommand();
-                sqlCommand.Transaction = sqlTransaction;
-                SqlParameter sqlParameter = new SqlParameter();
-                sqlParameter.ParameterName = "@nameDivision";
-                sqlParameter.Value = id;
-                sqlCommand.Parameters.Add(sqlParameter);
-                try
-                {
-                    sqlCommand.CommandText = "INSERT INTO Division " +
-                        "VALUES (@nameDivision)";
-                    sqlCommand.ExecuteNonQuery();
-                    sqlTransaction.Commit();
-                    Console.WriteLine($"Success membuat divisi {id} !!!");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                    Console.WriteLine("Input tidak valid");
-
-                }
+                return View("_ErorPage");
             }
-            return Ok();
+            Console.WriteLine($"{data.Id} - {data.DivisionName}");
+
+            return View("Index", new[] { data });
         }
-        public IActionResult DeleteDivision(int id)
+
+        public IActionResult AddDivision(Division division)
         {
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            if (ModelState.IsValid)
             {
-                conn.Open();
-                SqlTransaction sqlTransaction = conn.BeginTransaction();
-
-                SqlCommand sqlCommand = conn.CreateCommand();
-                sqlCommand.Transaction = sqlTransaction;
-                SqlParameter sqlParameter = new SqlParameter();
-                sqlParameter.ParameterName = "@id";
-                sqlParameter.Value = id;
-                sqlCommand.Parameters.Add(sqlParameter);
-                try
+                myContext.Divisions.Add(division);
+                var result = myContext.SaveChanges();
+                if (result > 0)
                 {
-                    sqlCommand.CommandText = "DELETE FROM Division " +
-                        "WHERE id=@id";
-                    sqlCommand.ExecuteNonQuery();
-                    sqlTransaction.Commit();
-                    Console.WriteLine("Success Menghapus divisi !!!");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                    Console.WriteLine("Input tidak valid");
-
+                    return RedirectToAction("Index");
                 }
             }
-            return Ok();
+
+            return View("_ErorPage");
         }
-        public IActionResult UpdateDivision(int id, string nameDivision)
+
+        public IActionResult DeleteDivision(Division division)
         {
-            using (SqlConnection conn = new SqlConnection(connectionString))
+            if (ModelState.IsValid)
             {
-                conn.Open();
-                SqlTransaction sqlTransaction = conn.BeginTransaction();
-
-                SqlCommand sqlCommand = conn.CreateCommand();
-                sqlCommand.Transaction = sqlTransaction;
-                SqlParameter sqlParameter = new SqlParameter();
-                sqlParameter.ParameterName = "@id";
-                sqlParameter.Value = id;
-                sqlCommand.Parameters.Add(sqlParameter);
-                SqlParameter sqlParameter2 = new SqlParameter();
-                sqlParameter2.ParameterName = "@nameDivision";
-                sqlParameter2.Value = nameDivision;
-                sqlCommand.Parameters.Add(sqlParameter2);
-                try
+                myContext.Divisions.Remove(division);
+                var result = myContext.SaveChanges();
+                if (result > 0)
                 {
-                    sqlCommand.CommandText = "UPDATE Division " +
-                        "SET name = @nameDivision WHERE id = @id;";
-                    sqlCommand.ExecuteNonQuery();
-                    sqlTransaction.Commit();
-                    Console.WriteLine($"Success mengubah divisi {nameDivision} !!!");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                    Console.WriteLine("Input tidak valid");
-
+                    return RedirectToAction("Index");
                 }
             }
-            return Ok();
+
+            return View("_ErorPage");
+        }
+        public IActionResult UpdateDivision(Division division)
+        {
+            if (ModelState.IsValid)
+            {
+                myContext.Divisions.Update(division);
+                var result = myContext.SaveChanges();
+                if (result > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View("_ErorPage");
+
         }
     }
 }
--- Models/Division.cs
+++ Models/Division.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +11,10 @@ namespace SimpleWebMVC.Models
     public class Division
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string DivisionName { get; set; }
     }
+    
 
 }
